4f4fbbf5deee3883639edb486f67dde4e324d290
Anna Berg <anbj21@student.bth.se>
2022-03-31 15:07:04 +0200

    KMOM01

--- src/Controller/Mine.php
+++ src/Controller/Mine.php
@@ -48,7 +48,18 @@ class Mine extends AbstractController
     public function Report(): Response
     {
         return $this->render('report.html.twig', [
-            'kmom1' => 'Skriv här',
+            'kmom1' => 'Jag har precis tidigare avslutat kursen objektorienterad python. Jag upplever verkligen att den kursen har hjälpt med förståelsen av objektorientering överlag.
+            Samt så höll vi även på med en del objektorientering i databaskursen, med ocjekt och moduler. Att kontinuerligt hålla på med sådana strukturer i kod hjälper en att förstå
+            övergripande drag i struktur oavsett språk. Det ät sättet att tänla som kontinuerligt utvecklas genom att arbeta med olika sätt att jobba. Överlag tycker jag även att nästan alla
+            kurser utom kanske första python kursen har varit till hjälp i denna kurs redan i kmom01. Det finns delar, moment och strukturer som man kontinuerligt känner igenom och blir
+            påmind om att kunna använda. Vilket är roligt. Överlag består ett objekt i php, av en klass som kan bestå av properties, konstanter och metoder. För att skapa sin första klass behöver
+            klassen innehålla någon av de tre punkterna. Utan ett innehåll och om klassen inte har några ärvda egenskaper så är klassen obrukbar då den inte har några egenskaper än existerande.
+            Först upplevde jag strukturen i symfony som otorligt svår. Jag försökte leta i filerna för att veta vart jag kunde jobba, utan framgång. Men då vände jag mig till symfony.com för att 
+            läsa referensmaterialen. Det underlättade hela arbetet. Efter hade jag en bredare förståelse och nu känns strukturen mer logisk. Jag fann ämnet "Interactions with Databases" som 
+            extra intressant i artikeln “PHP The Right Way”. Genom att föstå den logiska delen i arbete vi gör nu i detta kursmoment. Jag hade ingen vetskap om att objektorienterad PHP, underlättar 
+            arbetat markant vid testning. Tyvärr stod det inte mycket om detta än några konkreta exempel med kodstrukturer. Men jag skulle tycka det var intressant om vi kunde gå igenom mer 
+            bakomliggande argument till varför val av viss struktur i kod underlätar för det generella arbetet. Om man gör så kan det leda till... Och så vidare. 
+            Mitt TIL för detta kursmoment är absolut att titta mer i referensmaterial, "kolla mer i intruktionsboken".',
             'kmom2' => 'Skriv här',
             'kmom3' => 'Skriv här',
             'kmom4' => 'Skriv här',
