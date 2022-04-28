96777657da8ceebe1e3dc4b2935c33f800748584
Anna Berg <anbj21@student.bth.se>
2022-04-28 15:17:19 +0200

    5.0.3

---
+++ tools/phpstan/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'aced3deb7a6b2299774a30994120839762c651e1',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'aced3deb7a6b2299774a30994120839762c651e1',
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.6.3',
+            'version' => '1.6.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'reference' => '6128620b98292e0b69ea6d799871d77163681c8e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
